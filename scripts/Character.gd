7697c8f950c82b6c6eba2bd84b657f57bd9cb100
WaiYanPaing209 <waiyanricky@gmail.com>
2023-02-19 20:17:13 +0630

    commit 1

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -1,20 +1,29 @@
 extends KinematicBody2D
 
 const acceleration = 700
-const max_speed = 130
-const friction = 500
+const max_speed = 150
+const friction = 600
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationtree = $AnimationTree
 onready var animationstate = animationtree.get("parameters/playback")
 onready var velocity = Vector2.ZERO
 
+enum {
+	MOVE,
+	ATTACK
+}
+var state = MOVE
 func _ready():
 	animationtree.active = true
 
 func _physics_process(delta):
-	move_state(delta)
-	
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state()
+			
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -24,6 +33,7 @@ func move_state(delta):
 	if input_vector != Vector2.ZERO:
 		animationtree.set("parameters/Idle/blend_position",input_vector)
 		animationtree.set("parameters/Walk/blend_position",input_vector)
+		animationtree.set("parameters/Attack/blend_position",input_vector)
 		animationstate.travel("Walk")
 		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	
@@ -31,3 +41,13 @@ func move_state(delta):
 		animationstate.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		state = ATTACK
+	
+func attack_state():
+	velocity = Vector2.ZERO
+	animationstate.travel("Attack")
+	
+func on_attack_animation_finished():
+	state = MOVE
